d62fc525d3cc13e4a32c5adc612a36c16e6cce66
Clark Mckenzie <clark.mckenzie@ostmodern.co.uk>
2013-12-13 01:38:47 +0000

    Add non persistent chat history

--- server.py
+++ server.py
@@ -11,7 +11,17 @@ def broadcast_data (sock, message):
                 # broken socket connection may be, chat client pressed ctrl+c for example
                 socket.close()
                 CONNECTION_LIST.remove(socket)
- 
+
+# Send data to client
+def send_data(sock, message):
+    for socket in CONNECTION_LIST:
+        if socket == sock:
+            try:
+                socket.send(message)
+            except :
+                socket.close()
+                CONNECTION_LIST.remove(socket) 
+
 if __name__ == "__main__":
 
     if(len(sys.argv) < 2) :
@@ -22,7 +32,10 @@ if __name__ == "__main__":
     CONNECTION_LIST = []
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
     PORT = int(sys.argv[1])
-     
+   
+    # Chat history
+    hist = ''
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this has no effect, why ?
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -46,8 +59,9 @@ if __name__ == "__main__":
                 CONNECTION_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
                  
-                broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
-             
+                broadcast_data(sockfd, "\n[%s:%s] entered room\n" % addr)
+                send_data(sockfd, hist)
+
             #Some incoming message from a client
             else:
                 # Data recieved from client, process it
@@ -56,10 +70,12 @@ if __name__ == "__main__":
                     # a "Connection reset by peer" exception will be thrown
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)                
+			msg = "\r" + '<' + str(sock.getpeername()) + '> ' + data
+			hist += msg
+                        broadcast_data(sock, msg)                
                  
                 except:
-                    broadcast_data(sock, "Client (%s, %s) is offline" % addr)
+                    broadcast_data(sock, "Client (%s, %s) is offline\n" % addr)
                     print "Client (%s, %s) is offline" % addr
                     sock.close()
                     CONNECTION_LIST.remove(sock)
